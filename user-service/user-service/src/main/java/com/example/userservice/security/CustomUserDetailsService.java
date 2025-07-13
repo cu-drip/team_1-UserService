4f275a01bf3235e1ad001ea0285b0363336ecf7c
Kirill Erkaev <130300723+ks2999@users.noreply.github.com>
2025-07-13 20:16:59 +0300

    Delete user-service directory

--- user-service/user-service/src/main/java/com/example/userservice/security/CustomUserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.example.userservice.security;
-
-import org.springframework.stereotype.Service;
- 
-@Service
-public class CustomUserDetailsService {
-    // реализация UserDetailsService
-} 
