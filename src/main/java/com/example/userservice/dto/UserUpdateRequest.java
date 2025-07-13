fa3c6f1aa0d8cbb6d90667296fb066efde509839
Kirill Erkaev <130300723+ks2999@users.noreply.github.com>
2025-07-13 18:25:39 +0300

    Ну точно последняя версия)

---
+++ src/main/java/com/example/userservice/dto/UserUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.example.userservice.dto;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateRequest {
+    private String name;
+    private String surname;
+    private String patronymic;
+    private String phoneNumber;
+    private String email;
+    private String dateOfBirth;
+    private Integer age;
+    private String sex;
+    private Float weight;
+    private Float height;
+    private String bio;
+    private String avatarUrl;
+} 
